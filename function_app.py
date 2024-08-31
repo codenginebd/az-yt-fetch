937ef0294853444ef291bac8414839498a414f08
Md Shariful Islam <codenginebd@gmail.com>
2024-09-01 02:00:17 +0600

    change

--- function_app.py
+++ function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import logging
-import pytubefix 
+import pytubefix
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -42,6 +42,6 @@ def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "A new deployment",
              status_code=200
         )
